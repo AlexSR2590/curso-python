77af4c6a0e05b94f10c6fc1a9a4e54093637d56b
Alexis Arturo <alexsr2590@gmail.com>
2020-11-20 19:03:11 -0600

    curso python variables

---
+++ 01-holamundo/comentarios.py
@@ -0,0 +1,7 @@
+# Esto me mostrara el texto hola mundo!! por consola
+print("Hola")
+print("mundo")
+print("!!")
+"""
+Este es un comentario multilinea, no se mostrará
+"""
---
+++ 02-variables-y-tipos/variables.py
@@ -0,0 +1,29 @@
+texto = "Master en python"
+texto2 = "Con Alexis"
+numero = 45
+decimal =6.7
+
+print(texto)
+print(texto2)
+print(numero)
+print(decimal)
+
+numero = 77
+decimal = 8.9
+print("----------------------------------")
+print(numero)
+print(decimal)
+
+# Concatenación
+nombre = "Alexis"
+apellido = "Solis"
+edad = "30"
+print(nombre + " " + apellido + " Edad:" + edad)
+
+print(" ")
+#Otra forma de concatenar
+print(f"{nombre} {apellido} Edad {edad}")
+
+print(" ")
+# Concatenar con format
+print("Hola mi nombre es {} y tengo {} años".format(nombre, edad))

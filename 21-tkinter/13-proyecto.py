761baa56032241e38382bfbea804cc76409949c2
Alexis Arturo <alexsr2590@gmail.com>
2021-01-04 13:06:35 -0600

    segunda y ultima parte del proyecto tkinter

--- 21-tkinter/13-proyecto.py
+++ 21-tkinter/13-proyecto.py
@@ -6,16 +6,18 @@ Crear un programa que tenga
 - Un menu (Inicio, Añadir, Información, salir)(Hecho)
 - Opcion de salir (Hecho)
 - Diferentes pantallas (Hecho)
-- Formulario de añadir productos
-- Guardar datos temporalmente
+- Formulario de añadir productos (Hecho)
+- Guardar datos temporalmente (Hecho)
 - Mostrar datos listados en la pantalla home
 
 """
 from tkinter import *
+from tkinter import ttk
 
 #Definir ventana
 ventana = Tk()
-ventana.geometry("500x500")
+#ventana.geometry("500x500")
+ventana.minsize(500, 500)
 ventana.title("Proyecto tkinter - Master en Python")
 ventana.resizable(0, 0)#Para dejar el tamaño de la ventana fija
 
@@ -29,12 +31,20 @@ def home():
 		pady=20
 	)
 	home_label.grid(row=0, column=0)
+	products_box.grid(row=2)
+
+	#Listar productos
+	for product in products:
+		if len(product) == 3:
+			product.append("added")
+			products_box.insert('', 0, text=product[0], values = (product[1]))
 
 	#Ocultar otras pantallas
+	add_frame.grid_remove()
 	add_label.grid_remove()
 	info_label.grid_remove()
 	data_label.grid_remove()
-
+	
 	return True
 
 def add():
@@ -48,7 +58,8 @@ def add():
 	)
 	add_label.grid(row=0, column=0, columnspan=4, sticky=NW)
 
-	#Campos del formulario
+	#Estilos campos del formulario
+	add_frame.grid(row=1)
 	add_name_label.grid(row=1, column=0, padx=5, pady=5, sticky=W)
 	add_name_entry.grid(row=1, column=1, padx=5, pady=5, sticky=W)
 
@@ -57,12 +68,30 @@ def add():
 
 	add_description_label.grid(row=3, column=0, padx=5, pady=5, sticky=NW)
 	add_description_entry.grid(row=3, column=1, padx=5, pady=5, sticky=W)
+	add_description_entry.config(
+		width=30,
+		height=5,
+		font=("Consolas", 12),
+		padx=15,
+		pady=15
+	)
+
+	add_separator.grid(row=4)
+
+	boton.grid(row=5, column=1, sticky=NW)
+	boton.config(
+		bg="green",
+		fg="white",
+		pady=5,
+		padx=15
+	)
 
 	#Ocultando otras pantallas
 	home_label.grid_remove()
 	info_label.grid_remove()
 	data_label.grid_remove()
-
+	products_box.grid_remove()
+	#products_table.grid_remove()
 
 	return True
 
@@ -78,31 +107,56 @@ def info():
 	data_label.grid(row=1, column=0)
 	
 	#Ocultando otras pantallas
+	add_frame.grid_remove()
 	home_label.grid_remove()
 	add_label.grid_remove()
+	products_box.grid_remove()
 	
 	return True
 
+def add_product():
+	products.append([
+		name_data.get(),
+		price_data.get(),
+		add_description_entry.get("1.0", "end-1c")
+	])
+	name_data.set("")
+	price_data.set("")
+	add_description_entry.delete("1.0", END)
+
+	home()	
+
 #Variables importantes
+products = []
 name_data= StringVar()
 price_data = StringVar()
 
 #Definir campo de pantalla Inicio
 home_label = Label(ventana, text="Inicio")
+#products_box = Frame(ventana, width=250)
+Label(ventana).grid(row=1)
+products_box = ttk.Treeview(height=12, columns=2)
+products_box.grid(row=1, column=0, columnspan=2)
+products_box.heading("#0", text='Producto', anchor=W)
+products_box.heading("#1", text="Precio", anchor=W)
 
-
-#Definir campo de pantalla Añador
+#Definir campo de pantalla Añadir
 add_label = Label(ventana, text="Añadir producto")
 
 #Campos del formulario
-add_name_label = Label(ventana, text="Nombre: ")
-add_name_entry = Entry(ventana, textvariable=name_data)
+add_frame = Frame(ventana)
+add_name_label = Label(add_frame, text="Nombre: ")
+add_name_entry = Entry(add_frame, textvariable=name_data)
 
-add_price_label = Label(ventana, text="Precio: ")
-add_price_entry = Entry(ventana, textvariable=price_data)
+add_price_label = Label(add_frame, text="Precio: ")
+add_price_entry = Entry(add_frame, textvariable=price_data)
 
-add_description_label = Label(ventana, text="Descripción: ")
-add_description_entry = Text(ventana)
+add_description_label = Label(add_frame, text="Descripción: ")
+add_description_entry = Text(add_frame)
+
+add_separator = Label(add_frame)
+
+boton = Button(add_frame, text="Guardar", command=add_product)
 
 #Definir campo de pantalla Información
 info_label = Label(ventana, text="Información")
